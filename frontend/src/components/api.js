c0e12f178833375dba8e84dd679629a3a2cd9f97
ShreyasJenner <shreyasjenner4@gmail.com>
2024-02-14 15:44:48 +0530

    image gallery added

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -7,9 +7,9 @@ const imageUrl = "http://localhost:8080/images/";
 
 export default function Images() {
 
-    const [image, setImage] = React.useState(0);
     const [array, setArray] = React.useState([]);
-
+    const [curr, setIndex] = React.useState(0);
+    
     const getImage = async () => {
         const res = await axios.get(apiUrl+"list");
         setArray(res.data.files);
@@ -19,31 +19,62 @@ export default function Images() {
         getImage()
     }, []);
 
-    function serveNextImage() {
-        setImage(image+1);
-        if(image>=49)
-            setImage(0);
-        console.log(imageUrl+array[image]);
+    function next() {
+        setIndex(curr+140);
     }
 
-    function servePrevImage() {
-        setImage(image-1);
-        if(image<=0)
-            setImage(49);
-        console.log(imageUrl+array[image]);
+    function prev() {
+        setIndex(curr-140);
     }
 
+    function random() {
+        const rand = Math.floor(Math.random()*100);
+        let i=1;
+        while(i<rand) {
+            next();
+            i++;
+        }
+    }
+
+    let angle = (array.length-2)*180;
+    angle = angle/array.length;
+
+    const images = array.map((image,index) => {
+        const styles= {
+            transform: `rotateY(calc(${index+curr}*${angle}deg)) translateZ(400px)`
+        };
+
+        const size = {
+            width: `100px`,
+            height: `100px`
+        };
+
+        return (
+            <>
+            <span style={styles}>
+                <img style={size} src={imageUrl+image} alt="image render" />
+            </span>
+            </>
+        );
+    });    
+
+    const galleryStyles = {
+        transform: `perspective(1000px) rotateY(${curr}deg)`
+    };
+    
     return (
         <>
-        <h1>{array[image]}</h1>
-        <div className='content'>
-            <button onClick={servePrevImage}>Prev</button>
-            <br></br>
-            <img className="image" src={imageUrl+array[image]} alt="image"/>
-            <br></br>
-            <button onClick={serveNextImage}>Next</button>
+        <div className="body">
+            <div className='gallery' style={galleryStyles}>
+                {images}
+            </div>
+
+            <div className="btn-container">
+                <div className="btn" id="prev" onClick={prev}>Prev</div>
+                <div className="btn" id="random" onClick={random}>Random</div>
+                <div className="btn" id="next" onClickCapture={next}>Next</div>
+            </div>
         </div>
         </>
-        
     );
 }
