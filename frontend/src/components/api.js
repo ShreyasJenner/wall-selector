384d6c01cd5ba082d9bc4aafb0723a6d0001f83f
ShreyasJenner <shreyasjenner4@gmail.com>
2024-02-16 16:54:58 +0530

    rotating gallery with flickering

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -9,7 +9,10 @@ export default function Images() {
 
     const [array, setArray] = React.useState([]);
     const [index, setIndex] = React.useState(0);
-    const [currImage, setCurr] = React.useState("");
+    const [enterImage, setEnter] = React.useState("");
+    const [exitImage, setExit] = React.useState("");
+    const [leftImage, setLeft] = React.useState("");
+    const [rightImage, setRight] = React.useState("");
     
     const getImage = async () => {
         const res = await axios.get(apiUrl+"list");
@@ -20,26 +23,39 @@ export default function Images() {
         getImage()
     }, []);
 
-    function ServeImage() {
-        setCurr(array[index]);
-        return (
-            <div className="image">
-                <img src={imageUrl+currImage} alt="image" />
-            </div>
-        )
-    }
-
+    
     function changeImage() {
         setIndex((index+1)%array.length);
     }
 
-    setInterval(changeImage,3000)
+    function ServeImage() {
+        setEnter(array[index]);
+
+        setLeft(array[index-1]===undefined?array[array.length-1]:array[index-1]);
+
+        setRight(array[index-2]===undefined?array[array.length-2]:array[index-2]);
+
+        return (
+            <>
+            <div className="image">
+                <img src={imageUrl+enterImage} alt="image" id="enter"/>
+                <img src={imageUrl+leftImage} alt="image" id="left"/>
+                <img src={imageUrl+rightImage} alt="image" id="right"/>
+            </div>
+            </>
+        )
+    }
+
+    const interval = setInterval(() => {
+        setIndex((index+1)%array.length);
+    }, 2000)
 
     return (
         <>
         <div className="main">
             <div className="gallery">
                 <ServeImage />
+                {interval}
             </div>
 
         <div className="btn-container">
